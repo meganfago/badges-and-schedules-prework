2a927f550742fea6d2795eae2041ac3f525953d5
Megan Fago <meganlaynefago@gmail.com>
2018-11-24 16:48:27 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,6 +3,7 @@ return "Hello, my name is #{name}."
  end
  
  def batch_badge_creator(attendees)
+   conference_speakers = []
    attendees.each do |attendee|
-  return "Hello, my name is #{attendees}."
+  return "Hello, my name is #{attendee}."
 end 
