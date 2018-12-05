f72f113df6b86d232edaa8d7fdffbde1ba2b1894
Megan Fago <meganlaynefago@gmail.com>
2018-12-05 00:50:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+def badge_maker(name)
+return "Hello, my name is #{name}."
+ end
+ 
+ def batch_badge_creator(attendees)
+   counter = 0
+   while (counter < 7)
+  return "Hello, my name is #{attendees[counter]}."
+  counter += 1
+end 
+end
